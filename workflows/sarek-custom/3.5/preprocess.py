4d8cc4180b588fd2203e7ab570d2935e8fabab57
Sam Minot <sminot@gmail.com>
2025-01-09 12:52:32 -0800

    Add sarek-custom

---
+++ workflows/sarek-custom/3.5/preprocess.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+
+from cirro.helpers.preprocess_dataset import PreprocessDataset
+import os
+import pandas as pd
+
+
+def make_manifest(ds: PreprocessDataset) -> pd.DataFrame:
+
+    # Filter out any index files that may have been uploaded
+    ds.files = ds.files.loc[
+        ds.files.apply(
+            lambda r: r.get('readType', 'R') == 'R',
+            axis=1
+        )
+    ]
+
+    # Make a wide manifest
+    manifest = ds.wide_samplesheet(
+        index=["sampleIndex", "sample", "lane", "dataset"],
+        columns="read",
+        values="file",
+        column_prefix="fastq_"
+    )
+    assert manifest.shape[0] > 0, "No files detected -- there may be an error with data ingest"
+
+    # append metadata to file paths
+    samples = ds.samplesheet.set_index("sample")
+    manifest: pd.DataFrame = manifest.assign(
+        **{k: manifest["sample"].apply(v.get) for k, v in samples.items()}
+    )
+    ordering = ['patient', 'sex', 'status', 'sample', 'lane', 'fastq_1', 'fastq_2']
+    manifest = manifest.reindex(columns=ordering)
+
+    # Overwrite the 'lane' column to provide a unique value per-row
+    # This is necessary to account for datasets which merge multiple flowcells
+    manifest = manifest.assign(lane=[
+        str(i)
+        for i in range(manifest.shape[0])
+    ])
+
+    # Set the default value for 'sex' to be NA
+    manifest = manifest.assign(
+        sex=manifest['sex'].fillna('NA')
+    )
+
+    # Transform status values "Normal" -> 0 and "Tumor" -> 1
+    manifest = manifest.replace(
+        to_replace=dict(
+            status=dict(
+                normal=0,
+                Normal=0,
+                tumor=1,
+                Tumor=1
+            )
+        )
+    )
+
+    # Set the default status to 0
+    manifest = manifest.assign(
+        status=manifest["status"].fillna(0).apply(int)
+    )
+
+    # Set the default "patient" attribute as the sample
+    manifest = manifest.assign(
+        patient=manifest["patient"].fillna(manifest["sample"])
+    )
+
+    return manifest
+
+
+if __name__ == "__main__":
+
+    # Load the information for this dataset
+    ds = PreprocessDataset.from_running()
+
+    # Make the samplesheet
+    manifest = make_manifest(ds)
+
+    # Log the manifest
+    for line in manifest.to_csv(index=None).split("\n"):
+        ds.logger.info(line)
+
+    # Write manifest
+    manifest.to_csv("manifest.csv", index=None)
+
+    assert ds.params.get("tools") is not None, "ERROR: You must select a variant calling tool."
+
+    # Convert the tools paramater from a list to a comma delimited string
+    ds.add_param('tools', ",".join(ds.params.get("tools")), overwrite=True)
+
+    # Dynamic Resource Usage
+
+    # `compute_multiplier` == 2 for WGS and 1 for WES
+    ds.add_param(
+        "compute_multiplier",
+        int(2 - int(ds.params.get('wes')))
+    )
+
+    # Make a stub within the custom_config_base/ folder which is used to import the config
+    os.makedirs("custom_config_base/pipeline/")
+    with open("custom_config_base/pipeline/sarek.config", "wt") as handle:
+        handle.write("""
+    profiles {
+        standard { includeConfig "%s" }
+    }""" % f"{os.getcwd()}/nextflow-override.config")
+    ds.add_param("custom_config_base", f"{os.getcwd()}/custom_config_base")
+    assert os.path.exists("custom_config_base/pipeline/sarek.config")
+    print("Added handle for compute config to custom_config_base/pipeline/sarek.config")
+
+    # If an intervals file was not selected, use --no_intervals
+    if not ds.params.get("intervals"):
+        ds.add_param("no_intervals", True)
+
+    # log all params
+    ds.logger.info(ds.params)
