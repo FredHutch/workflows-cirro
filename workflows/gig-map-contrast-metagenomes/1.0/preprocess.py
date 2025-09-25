fcf5fa33c268da3471dc53d87d8a700be2278601
Sam Minot <sminot@gmail.com>
2025-09-25 12:09:48 -0700

    Find the gene_bins.csv from the input dataset

--- workflows/gig-map-contrast-metagenomes/1.0/preprocess.py
+++ workflows/gig-map-contrast-metagenomes/1.0/preprocess.py
@@ -1,11 +1,37 @@
 
+import json
+import os
 from cirro.helpers.preprocess_dataset import PreprocessDataset
+from subprocess import call
 
 # Instantiate the Cirro dataset object
 ds = PreprocessDataset.from_running()
 
-# Write out the sample metadata as a CSV
-ds.samplesheet.to_csv("metadata.csv", index=None)
+
+def find_gene_bins(ds: PreprocessDataset, data_path="/data/bin_pangenome/gene_bins.csv") -> str:
+    # Get the input dataset
+    input = ds.metadata['inputs'][0]
+
+    # If the input dataset is annotated with the pangenome used to create it
+    if "params" in input and "inputs" in input["params"] and "pangenome" in input["params"]["inputs"]:
+        return input["params"]["inputs"]["pangenome"] + data_path
+
+    # If the dataset was uploaded directly
+    else:
+        # There should be a file present called "pangenome_info.json"
+        print("Downloading pangenome info")
+        call(["aws", "s3", "cp", input["s3"] + "/data/pangenome_info.json", "./"])
+        assert os.path.exists("pangenome_info.json")
+        pangenome_info = json.load(open("pangenome_info.json"))
+
+        # Construct the path to the gene bins that are referenced
+        s3_base = ds.metadata['dataset']['s3'].rsplit("/", 1)[0]
+        return f"{s3_base}/{pangenome_info['uuid']}{data_path}"
+
+
+# Get the gene_bins.csv path by inspecting the input dataset
+gene_bins = find_gene_bins(ds)
+ds.add_param("gene_bins", gene_bins)
 
 # Log the parameters present
 for k, v in ds.params.items():
